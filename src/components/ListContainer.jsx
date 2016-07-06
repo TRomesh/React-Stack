8e47c0b423e55dfef1dbe23de32c36ac5ebd81ea
Tharaka Romesh Hewavitharana <trhewavitharana@virtusapolaris.com>
2016-07-06 12:05:37 +0530

    added dependencies react react-dom babel-core babel-loader babel-preset-react babel-preset-es2015

---
+++ src/components/ListContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ListItem from './ListItem.jsx';
+
+class ListContainer extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>Heyyy</h1>
+              <ListItem/>
+         </div>
+      );
+   }
+}
+
+export default ListContainer;
---
+++ src/components/ListItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+var boxStyle ={
+  height:200,
+  width:700,
+  backgroundColor: 'lightblue'
+};
+
+class ListItem extends React.Component {
+
+
+   render() {
+      return (
+         <div style={boxStyle}>
+
+         </div>
+      );
+   }
+}
+
+export default ListItem;
